580203380b53877e29b00da2834c37e4f30b3bc5
akashgupta744 <akgupta427@gmail.com>
2023-11-28 11:47:03 +0530

    project06
    url navigation

---
+++ project06/app01/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Aksh(request):
+    return render(request,'aksh.html')
+def Akash(request):
+    return render(request,'akash.html')
+def Akku(request):
+    return render(request,'akku.html')
