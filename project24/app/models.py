b698be76829c763db067383ee4f6a58480b2ea18
akashgupta744 <akgupta427@gmail.com>
2024-01-29 20:42:35 +0530

    project 24 ~ updation1
    joins ORM

---
+++ project24/app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Dept(models.Model):
+    deptno = models.IntegerField(primary_key = True)
+    dname = models.CharField(max_length = 100)
+    loc = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.dname
+
+class Emp(models.Model):
+    empno = models.IntegerField(primary_key = True)
+    ename = models.CharField(max_length = 100)
+    job  = models.CharField(max_length = 100)
+    mgr = models.IntegerField(self, on_delete = models.SET_NULL, null = True, blank = True)
+    hiredate = models.DateField()
+    sal = models.DecimalField(max_digit = 10, decimal_place = 2)
+    comm = models.DecimalField(max_digit = 10, decimal_place = 2)
+    deptno = models.ForeignKey(Dept, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ename
+
+class Salgrade(models.Model):
+    grade = models.IntegerField(primary_key = True)
+    lsal = models.DecimalField(max_digit = 10, decimal_place = 2)
+    hsal = models.DecimalField(max_digit = 10, decimal_place = 2)
+
+    def __str__(self):
+        return self.grade
+
